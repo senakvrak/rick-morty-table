ca73fda6c9a87bfafcf9a6601411978a9cdc35c0
senakvrak <35231465+senakvrak@users.noreply.github.com>
2024-07-12 21:11:01 +0300

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from "react";
 import CharacterTable from "./components/CharacterTable";
 
+// App bileşeni, uygulamanın ana bileşenidir ve burada diğer bileşenler çağrılır.
 const App = () => {
   return (
     <div>
