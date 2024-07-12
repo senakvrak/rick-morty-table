fd6b437c4e1d6f083265c7e5c8fe123af178df66
senakvrak <35231465+senakvrak@users.noreply.github.com>
2024-07-12 21:03:25 +0300

    Update api.js

--- src/api.js
+++ src/api.js
@@ -1,13 +1,17 @@
 import axios from "axios";
 
+// API URL'si, Rick and Morty API'sinin karakter verilerini sağlar.
 const API_URL = "https://rickandmortyapi.com/api/character/";
 
+// Karakter verilerini getiren asenkron fonksiyon.
+// 'page' parametresi ve diğer filtreler fonksiyona argüman olarak geçirilir.
 export const fetchCharacters = async (
   page = 1,
   pageSize = 10,
   filters = {}
 ) => {
   try {
+    // API'ye GET isteği gönderilir ve sonuç döndürülür.
     const response = await axios.get(API_URL, {
       params: { page, pageSize, ...filters },
     });
